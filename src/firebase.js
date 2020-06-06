a95726843dae2e72a1ad38e5eef4fcbd4b9951d9
Adeola J. Adeyemo <adeola.adeyemoj@yahoo.com>
2020-06-06 21:15:34 +0100

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebase from '@firebase/app';
-import '@firebase/functions';
-
-// Firebase config from env
-const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
-
-const reactFirebase = firebase.initializeApp(config)
-export default reactFirebase
